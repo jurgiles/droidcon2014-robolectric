79f8532ef3c9b25cd5e631ec2073cfc0d694b361
Johny Urgiles <johny@augmate.com>
2014-09-20 23:40:35 -0400

    fix jdk and updated formatting for size 28ft

--- app/src/androidTest/java/com/jurgiles/sdk/droidcon2014/app/MainActivityTest.java
+++ app/src/androidTest/java/com/jurgiles/sdk/droidcon2014/app/MainActivityTest.java
@@ -34,37 +34,53 @@ public class MainActivityTest {
 
     @Test
     public void shouldCreateActivityWithHelloTextWithoutFEST() {
-        ActivityController<MainActivity> controller = ActivityController.of(MainActivity.class);
-        MainActivity activity = controller.create().get();
+        MainActivity activity = ActivityController
+                                    .of(MainActivity.class)
+                                    .create()
+                                    .get();
 
-        TextView helloDroidconView = (TextView) activity.findViewById(R.id.hello_droidcon_text);
+        TextView helloDroidconView =
+                (TextView) activity.findViewById(R.id.hello_droidcon_text);
 
         assertTrue("Hello Droidcon!".equals(helloDroidconView.getText()));
     }
 
     @Test
     public void shouldCreateActivityWithHelloText() {
-        ActivityController<MainActivity> controller = ActivityController.of(MainActivity.class);
-        MainActivity activity = controller.create().get();
+        MainActivity activity = ActivityController.
+                                    of(MainActivity.class)
+                                    .create()
+                                    .get();
 
-        TextView helloDroidconView = (TextView) activity.findViewById(R.id.hello_droidcon_text);
+        TextView helloDroidconView =
+                (TextView) activity.findViewById(R.id.hello_droidcon_text);
 
         ANDROID.assertThat(helloDroidconView).containsText("Hello Droidcon!");
     }
 
     @Test
     public void shouldChangeTextWhenButtonPressed() {
-        MainActivity activity = ActivityController.of(MainActivity.class).create().visible().get();
+        MainActivity activity = ActivityController
+                                    .of(MainActivity.class)
+                                    .create()
+                                    .visible()
+                                    .get();
 
         Robolectric.clickOn(activity.findViewById(R.id.press_me_button));
 
-        TextView helloDroidconView = (TextView) activity.findViewById(R.id.hello_droidcon_text);
+        TextView helloDroidconView =
+                (TextView) activity.findViewById(R.id.hello_droidcon_text);
+
         ANDROID.assertThat(helloDroidconView).containsText("Dance Dance Dance");
     }
-    
+
     @Test
     public void shouldCloseActivityWhenButtonIsPressed() {
-        MainActivity activity = ActivityController.of(MainActivity.class).create().visible().get();
+        MainActivity activity = ActivityController
+                                    .of(MainActivity.class)
+                                    .create()
+                                    .visible()
+                                    .get();
 
         Robolectric.clickOn(activity.findViewById(R.id.big_red_button));
 
@@ -73,56 +89,101 @@ public class MainActivityTest {
 
     @Test
     public void shouldLaunchNewActivityWhenBigRedButtonPressed() {
-        MainActivity activity = ActivityController.of(MainActivity.class).create().visible().get();
+        MainActivity activity = ActivityController
+                                    .of(MainActivity.class)
+                                    .create()
+                                    .visible()
+                                    .get();
 
         Robolectric.clickOn(activity.findViewById(R.id.big_red_button));
 
-        Intent nextStartedActivity = Robolectric.getShadowApplication().getNextStartedActivity();
-        ANDROID.assertThat(nextStartedActivity).hasComponent(Robolectric.application, BrandNewActivity.class);
+        Intent nextStartedActivity = Robolectric.getShadowApplication()
+                                        .getNextStartedActivity();
+
+        ANDROID.assertThat(nextStartedActivity)
+                .hasComponent(Robolectric.application, BrandNewActivity.class);
     }
 
     @Test
     public void shouldLaunchWelcomeNotificationOnStart() {
-        ActivityController.of(MainActivity.class).create().visible().start();
+        ActivityController.of(MainActivity.class)
+                .create()
+                .visible()
+                .start();
 
-        NotificationManager notificationManager = (NotificationManager) Robolectric.application.getSystemService(Context.NOTIFICATION_SERVICE);
-        List<Notification> allNotifications = Robolectric.shadowOf(notificationManager).getAllNotifications();
+        NotificationManager notificationManager =
+                (NotificationManager)
+                        Robolectric.application
+                            .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        List<Notification> allNotifications =
+                Robolectric.shadowOf(notificationManager)
+                        .getAllNotifications();
 
         assertThat(allNotifications).hasSize(1);
     }
 
     @Test
     public void shouldLaunchWelcomeNotificationOnStartWithStuff() {
-        ActivityController.of(MainActivity.class).create().visible().start().get();
+        ActivityController.of(MainActivity.class)
+                .create()
+                .visible()
+                .start()
+                .get();
 
-        NotificationManager notificationManager = (NotificationManager) Robolectric.application.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = Robolectric.shadowOf(notificationManager).getNotification(null, 123);
+        NotificationManager notificationManager =
+                (NotificationManager)
+                        Robolectric.application
+                            .getSystemService(Context.NOTIFICATION_SERVICE);
 
-        ShadowNotification shadowNotification = Robolectric.shadowOf(notification);
-        assertThat(shadowNotification.getContentTitle()).isEqualTo("Dance Party");
-        assertThat(shadowNotification.getContentText()).isEqualTo("Unce Unce Unce");
+        Notification notification =
+                Robolectric.shadowOf(notificationManager)
+                        .getNotification(null, 123);
+
+        ShadowNotification shadowNotification =
+                Robolectric.shadowOf(notification);
+
+        assertThat(shadowNotification.getContentTitle())
+                .isEqualTo("Dance Party");
+        assertThat(shadowNotification.getContentText())
+                .isEqualTo("Unce Unce Unce");
     }
 
     @Test
     public void shouldKickoffAsyncTaskOnStart() {
         Robolectric.getBackgroundScheduler().pause();
 
-        ActivityController.of(MainActivity.class).create().visible().start().get();
+        ActivityController.of(MainActivity.class)
+                .create()
+                .visible()
+                .start()
+                .get();
 
-        assertEquals(Robolectric.getBackgroundScheduler().enqueuedTaskCount(), 1);
+        assertEquals(Robolectric.getBackgroundScheduler()
+                .enqueuedTaskCount(), 1);
 
         Robolectric.runBackgroundTasks();
 
-        assertThat(Robolectric.getBackgroundScheduler().size()).isEqualTo(0);
+        assertThat(Robolectric.getBackgroundScheduler().size())
+                .isEqualTo(0);
     }
 
     @Test
     public void shouldGetLatestSongFromNetworkAndUpdatesUI() {
-        ActivityController.of(MainActivity.class).create().visible().start().get();
+        ActivityController.of(MainActivity.class)
+                .create()
+                .visible()
+                .start()
+                .get();
+
         Robolectric.runBackgroundTasks();
 
-        RequestLine requestLine = Robolectric.getNextSentHttpRequest().getRequestLine();
-        assertThat(requestLine.getMethod()).isEqualTo("GET");
-        assertThat(requestLine.getUri()).isEqualTo("www.danceparty.com/bumpin");
+        RequestLine requestLine = Robolectric.getNextSentHttpRequest()
+                                    .getRequestLine();
+
+        assertThat(requestLine.getMethod())
+                .isEqualTo("GET");
+        assertThat(requestLine.getUri())
+                .isEqualTo("www.danceparty.com/bumpin");
     }
 }
